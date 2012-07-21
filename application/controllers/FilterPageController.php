f396e03093dde792351186e80a634e9463b9a84c
Marlin Cremers <marlinc@mms-projects.net>
2012-07-21 15:36:47 +0200

    Moved the filter sidebar widget to the view

--- application/controllers/FilterPageController.php
+++ application/controllers/FilterPageController.php
@@ -6,12 +6,6 @@ class FilterPageController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-        $this->view->sidebar()->prepend(
-            $this->view->filterWidget(), $this->view->translate('World filter')
-        );
-        $this->view->headScript()->appendFile(
-            $this->view->baseUrl('scripts/filter.js')
-        );
     }
 
     public function indexAction()
