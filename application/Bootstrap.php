6358a6cb357e1c80a19b83c922c15d3b052be4b5
Marlin Cremers <marlinc@mms-projects.net>
2012-07-21 12:19:07 +0200

    The error page now uses translations and the translations are now defined in the config

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,20 +2,6 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
-    public function _initTranslations()
-    {
-        $adapter = new Zend_Translate(
-            array(
-                'adapter' => 'csv',
-                'content' => '../languages',
-                'scan'    => Zend_Translate::LOCALE_FILENAME,
-                'locale'  => 'nl'
-            )
-        );
-        Zend_Registry::set('Zend_Translate', $adapter);
-    }
     
     public function _initNavigation()
     {
