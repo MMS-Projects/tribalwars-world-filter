d71dfc414e1691a77584eaf470bc48649b9027f6
Marlin Cremers <marlinc@mms-projects.net>
2012-07-27 20:08:28 +0200

    The language-bar flags now use the base-url helper to get the right URI

--- application/views/helpers/LanguageBar.php
+++ application/views/helpers/LanguageBar.php
@@ -17,7 +17,7 @@ class Zend_View_Helper_LanguageBar extends Zend_View_Helper_Placeholder_Containe
                 'http://%s.%s%s', $country, $domain, $this->view->url()
             );
             $flag = sprintf(
-                'http://%s/flags/%s.gif', $domain, $country
+                $this->view->baseUrl('flags/%s.gif'), $country
             );
             $html .= sprintf(
                 '<li><a href="%s"><img src="%s"> %s</a></li>', $url, $flag,
