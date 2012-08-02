e11d731810f28cd78ce6499fa12ee9dbd73d1f08
Marlin Cremers <marlinc@mms-projects.net>
2012-08-02 22:48:59 +0200

    Added a name to the world model Added the local jQuery plugin to the colour layout

--- application/models/World.php
+++ application/models/World.php
@@ -1,10 +1,12 @@
 <?php
 class Application_Model_World {
+    public $name;
 	public $speed;
 	public $unitspeed;
 	
 	public function randomize ()
 	{
+	    $this->name      = 'Wereld ' . rand(1, 20);
 		$this->speed     = 1.0 + rand(0, 2) * 0.5;
 		$this->unitspeed = rand(1, 3) * (1 / 3);
 	}
