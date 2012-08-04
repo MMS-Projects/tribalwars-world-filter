86e3c8898d12a663ccfcaa36447823a774d54d20
Marlin Cremers <marlinc@mms-projects.net>
2012-08-04 21:46:21 +0200

    Added a 'get-filters' action to the AJAX controller

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -3,6 +3,68 @@
 class AjaxController extends Zend_Controller_Action
 {
 	public $worlds;
+	public $filters = array(
+	    array(
+	        'title'  => 'With or without paladin?',
+	        'id'     => 'paladin',
+	        'options' => array(
+	            array(
+	                'text'  => 'With paladin',
+	                'value' => 1
+	            ),
+	            array(
+	                'text'  => 'Without paladin',
+	                'value' => 2
+	            )
+	        )
+	    ),
+	    array(
+	        'title'  => 'Nobel item',
+	        'id'     => 'nobel-item',
+	        'options' => array(
+	            array(
+	                'text'  => 'Packages',
+	                'value' => 1
+	            ),
+	            array(
+	                'text'  => 'Coins',
+	                'value' => 2
+	            )
+	        )
+	    ),
+	    array(
+	        'title'  => 'With or without church?',
+	        'id'     => 'church',
+	        'options' => array(
+	            array(
+	                'text'  => 'With church',
+	                'value' => 1
+	            ),
+	            array(
+	                'text'  => 'Without church',
+	                'value' => 2
+	            )
+	        )
+	    ),
+	    array(
+	        'title'  => 'World speed?',
+	        'id'     => 'speed',
+	        'options' => array(
+	            array(
+	                'text'  => '1',
+	                'value' => 1
+	            ),
+	            array(
+	                'text'  => '1.5',
+	                'value' => 1.5
+	            ),
+	            array(
+	                'text'  => '2',
+	                'value' => 2
+	            )
+	        )
+	    ),
+    );
 	
     public function init()
     {
@@ -12,8 +74,17 @@ class AjaxController extends Zend_Controller_Action
     
     public function filterWorldsAction()
     {
-		$this->view->worlds = $this->worlds;
+        $parameters = $_POST;
+        $bla->uhuh = print_r($parameters, true);
+		
+		$this->view->worlds = array($bla);
+		$this->view->worlds += $this->worlds;
     }
+    
+    public function getFiltersAction()
+    {
+        $this->view->filters = $this->filters;
+    }    
     
     public static function getAvailableWorlds ()
     {
