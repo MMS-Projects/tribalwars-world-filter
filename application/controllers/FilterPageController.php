dc30fdd95997e7047bcad5c775a8b183dc6595e9
Marlin Cremers <marlinc@mms-projects.net>
2012-08-11 18:35:06 +0200

    Added a Facebook layout for the filter-page

--- application/controllers/FilterPageController.php
+++ application/controllers/FilterPageController.php
@@ -11,6 +11,9 @@ class FilterPageController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        if ($this->_hasParam('fb')) {
+            $this->view->layout()->setLayout('twf-fb');
+        }
     }
 
 
