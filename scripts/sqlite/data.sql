594cd2601ecc4cffe33f1404f365c7bd93bd7ba1
Marlin Cremers <marlinc@mms-projects.net>
2012-08-20 22:42:14 +0200

    Fixed the 'old-data' bug in the database

--- scripts/sqlite/data.sql
+++ scripts/sqlite/data.sql
@@ -2,23 +2,23 @@
 --
 -- You can begin populating the database with the following SQL statements.
  
-INSERT INTO "filters" VALUES(1,'Paladin','paladin');
-INSERT INTO "filters" VALUES(2,'Nobel item','nobel-item');
-INSERT INTO "filters" VALUES(3,'Church','church');
+INSERT INTO "filters" VALUES(1,'Paladin','game_knight');
+INSERT INTO "filters" VALUES(2,'Nobel item','snob_gold');
+INSERT INTO "filters" VALUES(3,'Church','game_church');
 INSERT INTO "filters" VALUES(4,'World speed','speed');
-INSERT INTO "filters" VALUES(5,'Bonux villages','bonus-villages');
+INSERT INTO "filters" VALUES(5,'Bonus villages','coord_bonus_villages');
 
 INSERT INTO "filter_options" VALUES(1,'2','2',4);
 INSERT INTO "filter_options" VALUES(2,'1.5','1.5',4);
 INSERT INTO "filter_options" VALUES(3,'1','1',4);
-INSERT INTO "filter_options" VALUES(4,'No','false',3);
-INSERT INTO "filter_options" VALUES(5,'Yes','true',3);
-INSERT INTO "filter_options" VALUES(6,'Coins','2',2);
-INSERT INTO "filter_options" VALUES(7,'Packages','1',2);
-INSERT INTO "filter_options" VALUES(8,'No','false',1);
-INSERT INTO "filter_options" VALUES(9,'Yes','True',1);
-INSERT INTO "filter_options" VALUES(10,'Yes','true',5);
-INSERT INTO "filter_options" VALUES(11,'No','false',5);
+INSERT INTO "filter_options" VALUES(4,'No','0',3);
+INSERT INTO "filter_options" VALUES(5,'Yes','1',3);
+INSERT INTO "filter_options" VALUES(6,'Coins','1',2);
+INSERT INTO "filter_options" VALUES(7,'Packages','0',2);
+INSERT INTO "filter_options" VALUES(8,'No','0',1);
+INSERT INTO "filter_options" VALUES(9,'Yes','1',1);
+INSERT INTO "filter_options" VALUES(10,'Yes','7',5);
+INSERT INTO "filter_options" VALUES(11,'No','0',5);
 
 INSERT INTO "worlds" VALUES(1,'nl1',0,0,0,1.0,0);
 INSERT INTO "worlds" VALUES(2,'nl2',1,1,0,2.0,0);
